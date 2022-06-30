837b1dd1b2395b54b671628200527f3c898c9ce4
juntuchen <juntuchen@microsoft.com>
2022-06-29 23:13:42 -0400

    updated

--- java_sdk_test_client/src/main/java/ContosoApp.java
+++ java_sdk_test_client/src/main/java/ContosoApp.java
@@ -12,10 +12,10 @@ import java.util.List;
 public class ContosoApp {
 
     public static void main(String[] args) {
-        String acsConnectionString = "endpoint=https://acstestappjuntu.communication.azure.com/;accesskey=pObSPL0lo5yica6qpe7tZ0kElcIFQUoQqZkM09DavShqvBArEMdK/G9MBuDRuftwPSjqYEX/yvmU9Z0WyVtEwA==";
-        String devPMAEndPoint = "https://pma-dev-juntuchen.plat-dev.skype.net";
-        String callbackUri = "http://419697f38d85.ngrok.io/OnIncomingCall";
-        CommunicationUserIdentifier target = new CommunicationUserIdentifier("8:acs:816df1ca-971b-44d7-b8b1-8fba90748500_00000012-2cf7-dbf2-bcc9-3e3a0d009d06");
+        String acsConnectionString = "";
+        String devPMAEndPoint = "";
+        String callbackUri = "";
+        CommunicationUserIdentifier target = new CommunicationUserIdentifier("");
 
         // Java SDK Initialization
         CallingServerClient callingServerClient = new CallingServerClientBuilder()
